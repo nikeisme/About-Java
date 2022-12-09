f0b7d88664e8aaa5f05bf5225028a7656f600011
nikeisme <nikeisme827@gmail.com>
2022-12-09 17:27:45 +0900

    재귀함수: 이진수 출력

---
+++ 인프런/DFS/BinaryNumberDFS.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.*;
+class Main {
+    public void DFS( int n) {
+
+        if (n == 0) return;
+        else {
+            DFS(n / 2);
+            System.out.println(n % 2 + " ");
+        }
+    }
+
+
+    public static void main(String[] args){
+        Main T = new Main();
+        T.DFS(3);
+    }
+}
