bd5b166033b9d26552e8d4fdee9f0fcc95c0bb6a
nikeisme <nikeisme827@gmail.com>
2022-12-24 18:35:27 +0900

    객체지향 프로그래밍1 이론을 위한 커밋

--- Java_Basic/객체지향프로그래밍1/TvTest4.java
+++ Java_Basic/객체지향프로그래밍1/TvTest4.java
@@ -3,7 +3,7 @@ class TvTest4 {
     public static void main (String args[]) {
         Tv[] tvArr = new Tv[3] ; // 길이가 3인 Tv 객체 배열
 
-        // Tv 객체를 생성해서 Tv 객체 배열의 각 요소에 저장
+
         for (int i = 0; i < tvArr.length; i++) {
             tvArr[i] = new Tv();
             tvArr[i].channel = i +10; // tvArr[i]의 channel에 i+10을 저장
