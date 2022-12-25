5d0ac15bfc6ce0d9aa4737baa8adba15f6195eae
nikeisme <nikeisme827@gmail.com>
2022-12-25 22:42:17 +0900

    객체지향프로그래밍 이론 탄탄히 쌓기

--- Java_Basic/객체지향프로그래밍1/TvTest4.java
+++ Java_Basic/객체지향프로그래밍1/TvTest4.java
@@ -1,16 +1,16 @@
 class TvTest4 {
 
     public static void main (String args[]) {
-        Tv[] tvArr = new Tv[3] ; // 길이가 3인 Tv 객체 배열
+        Tv[] tvArr = new Tv[3] ;
 
 
         for (int i = 0; i < tvArr.length; i++) {
             tvArr[i] = new Tv();
-            tvArr[i].channel = i +10; // tvArr[i]의 channel에 i+10을 저장
+            tvArr[i].channel = i +10;
         }
 
         for (int i = 0; i < tvArr.length; i++) {
-            tvArr[i].channelUp(); // tvArr[i]의 메서드를 호출. 채널 1이 증가
+            tvArr[i].channelUp();
             System.out.printf("tvArr[%d].channel=%d%n", i, tvArr[i].channel);
         }
     }
