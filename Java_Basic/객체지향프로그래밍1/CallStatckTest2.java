0b1cc807f68ddd6b17a3f04225e63bda2d7d5fa7
nikeisme <nikeisme827@gmail.com>
2022-12-30 23:13:38 +0900

    객체지향프로그래밍1 / JVM 호출 이론 복습

--- Java_Basic/객체지향프로그래밍1/CallStatckTest2.java
+++ Java_Basic/객체지향프로그래밍1/CallStatckTest2.java
@@ -1,20 +1,20 @@
 public class CallStatckTest2 {
 
     public static void main (String args[]) {
-        System.out.println("main (String args) 이 시작되었음.");
+        System.out.println("main (String args) 시작.");
         firstMethod();
-        System.out.println("main (String args) 이 끝났음.");
+        System.out.println("main (String args) 끝.");
     }
 
     static void firstMethod() {
-        System.out.println("firstMethod () 이 시작되었음.");
+        System.out.println("firstMethod () 시작.");
         secondMethod();
-        System.out.println("firstMethod () 이 끝났음.");
+        System.out.println("firstMethod () 끝.");
     }
 
     static void secondMethod() {
-        System.out.println("secondMethod( ) 이 시작되었음.");
-        System.out.println("secondMethod( ) 이 끝났음.");
+        System.out.println("secondMethod( ) 시작.");
+        System.out.println("secondMethod( ) 끝.");
     }
 
 }
