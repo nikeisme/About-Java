377016bafce96a2ac5064bd8096d203aec43bf86
nikeisme <nikeisme827@gmail.com>
2023-01-01 23:07:01 +0900

    객체지향프로그래밍 / 재복습

--- Java_Basic/객체지향프로그래밍1/CardTest.java
+++ Java_Basic/객체지향프로그래밍1/CardTest.java
@@ -2,15 +2,13 @@ public class CardTest {
 
     public static void main (String args[]) {
 
-        // 클래스 변수(Static 변수)는 객체 생성없이 '클래스이름.클래스 변수'로 직접 사용이 가능하다.
         System.out.println("Card.width =" + Card.width);
         System.out.println("Card.height =" + Card.height);
 
         Card c1 = new Card();
         c1.kind = "Heart";
         c1.number = 7;
-
-        // 인스턴스 변수의 값을 변경한다.
+        
         Card c2 = new Card();
         c2.kind = "Spade";
         c2.number = 4;
