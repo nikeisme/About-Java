49cfa93503a7c6d9f1a6733f7eba551505cb6798
nikeisme <nikeisme827@gmail.com>
2023-01-03 23:19:26 +0900

    객체지향프로그래밍 1 : 인스턴스 변수 재복습

--- Java_Basic/객체지향프로그래밍1/MemberCall.java
+++ Java_Basic/객체지향프로그래밍1/MemberCall.java
@@ -4,12 +4,12 @@ public class MemberCall {
     static int cv = 20;
 
     int iv2 = cv;
-    //    static int cv2 = iv; // 에러. 클래스변수는 인스턴스 변수를 사용할 수 없음.
+    // static int cv2 = iv; // 에러 발생 클래스변수는 인스턴스 변수를 사용할 수 없음.
     static int cv2 = new MemberCall().iv; // 이처럼 객체를 생성해야 사용 가능.
 
     static void staticMethod() {
         System.out.println(cv);
-//        System.out.println(iv); // 결과 : 에러. 클래스메서드에서 인스턴스 변수를 사용 불가
+//        System.out.println(iv); // 결과 : 에러 발생 클래스메서드에서 인스턴스 변수를 사용 불가
         MemberCall c = new MemberCall();
         System.out.println(c.iv) ; // 객체를 생성한 후에야 인스턴스 변수 참조 가능
     }
