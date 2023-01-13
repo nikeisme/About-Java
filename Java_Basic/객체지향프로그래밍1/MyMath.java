402fd6e941118b666b6baff66316281e5a499bb9
nikeisme <nikeisme827@gmail.com>
2023-01-13 17:08:27 +0900

    객체지향프로그래밍: 생성자 복습

--- Java_Basic/객체지향프로그래밍1/MyMath.java
+++ Java_Basic/객체지향프로그래밍1/MyMath.java
@@ -3,8 +3,6 @@ public class MyMath {
     long add(long a, long b) {
         long result = a + b;
         return result;
-
-        // return a + b; // 위의 두 줄을 이와 같이 한 줄로 간단히 할 수 있다.
     }
 
     long subtract(long a, long b) {
